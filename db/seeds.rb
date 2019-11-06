268825dde197078f7faa1b906edecc23c634200f
David Garcia <davidmd.garcia@gmail.com>
2019-11-06 17:20:38 +0900

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant1 = Restaurant.create(name: "Wagon Sushi", phone_number: "000000000", address: "Meguro", category: "japanese")
+restaurant2 = Restaurant.create(name: "Napoli", phone_number: "000000000", address: "Shinjuku", category: "italian" )
+restaurant3 = Restaurant.create(name: "Lugdunum", phone_number: "000000000", address: "Idabashi", category: "french" )
+restaurant4 = Restaurant.create(name: "Chez Jean-Paul", phone_number: "000000000", address: "Bruxelles", category: "belgian" )
+restaurant5 = Restaurant.create(name: "Lunchy", phone_number: "000000000", address: "Paris", category: "french" )
